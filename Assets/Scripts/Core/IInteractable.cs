b668d96cb0f8f0c128629f6f8b0b3140a04ace4a
murtiturti <murat.diken@mail.utoronto.ca>
2023-12-20 15:21:26 -0500

    Base of the interaction system implemented

---
+++ Assets/Scripts/Core/IInteractable.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AriozoneGames.Core
+{
+    public interface IInteractable
+    {
+        InteractType InteractType { get; set; }
+        void Interact();
+    }
+}
---
+++ Assets/Scripts/Core/PlayerInteractManager.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+namespace AriozoneGames.Core
+{
+    public class PlayerInteractManager : MonoBehaviour
+    {
+        #region References
+        private CinemachineBrain _cinemachineBrain;
+        private Camera _playerCamera;
+        #endregion
+
+        #region RaycastVariables
+        [SerializeField] private float range = 10f;
+        #endregion
+
+        #region InteractVariables
+        private bool _isHandsFull = false;
+        private bool _interactableSelected = false;
+        private bool _isFrameOfSelection = false;
+        #endregion
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+            _cinemachineBrain = FindObjectOfType<CinemachineBrain>();
+            _playerCamera = _cinemachineBrain.OutputCamera;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            var interactable = SelectInteractable();
+            if (_interactableSelected)
+            {
+                if (_isFrameOfSelection)
+                {
+                    //TODO: Send message to UI manager to show prompt
+                    _isFrameOfSelection = false;
+                }
+
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    if (CheckInteractConditions())
+                    {
+                        InteractWith(interactable);
+                    }
+                }
+            }
+        }
+
+        private void InteractWith(IInteractable interactable)
+        {
+            interactable.Interact();
+            if (interactable.InteractType == InteractType.Pickup)
+            {
+                _isHandsFull = true;
+            }
+        }
+
+
+        private IInteractable SelectInteractable()
+        {
+            IInteractable go = null;
+            var ray = _playerCamera.ScreenPointToRay(new Vector3(Screen.width / 2f, Screen.height / 2f, 0f));
+            Debug.DrawRay(ray.origin, ray.direction * range, Color.blue);
+            if (Physics.Raycast(ray, out RaycastHit hit, range))
+            {
+                var interactable = hit.collider.GetComponent<IInteractable>();
+                if (interactable != null)
+                {
+                    go = interactable;
+                    _interactableSelected = true;
+                    if (!_isFrameOfSelection)
+                    {
+                        _isFrameOfSelection = true;
+                    }
+                    else
+                    {
+                        _isFrameOfSelection = false;
+                    }
+                }
+            }
+            return go;
+        }
+
+        private bool CheckInteractConditions()
+        {
+            //TODO: Update this method to take the interact type and check conditions for each type
+            return !_isHandsFull && _interactableSelected;
+        }
+        
+    }
+}
